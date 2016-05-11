e376207b33e84483f23f1e10e4079d2657129016
myun2 <myun2@nwhite.info>
2016-05-11 20:24:41 +0900

    Refactoring unpack

--- unpack.rb
+++ unpack.rb
@@ -3,57 +3,74 @@
 require 'yaml'
 require 'zlib'
 require_relative 'rgss'
-[
-  'Data/Actors.rvdata2',
-  'Data/Animations.rvdata2',
-  'Data/Armors.rvdata2',
-  'Data/Classes.rvdata2',
-  'Data/CommonEvents.rvdata2',
-  'Data/Enemies.rvdata2',
-  'Data/Items.rvdata2',
-  *Dir.glob('Data/Map[0-9][0-9][0-9].rvdata2'),
-  'Data/MapInfos.rvdata2',
-  'Data/Skills.rvdata2',
-  'Data/States.rvdata2',
-  'Data/System.rvdata2',
-  'Data/Tilesets.rvdata2',
-  'Data/Troops.rvdata2',
-  'Data/Weapons.rvdata2'
-].each do |rvdata2|
-  data = ''
-  puts "Unpacking... #{rvdata2} (to yml)"
-  File.open(rvdata2, 'rb') do |file|
-    data = Marshal.load(file.read)
-  end
-  File.open('Data/'+File.basename(rvdata2,'.rvdata2')+'.yml', 'w') do |file|
-    file.write(YAML.dump(data))
+
+def load_rvdata2(file)
+  Marshal.load(File.open(file, 'rb'))
+end
+
+def save_yaml(file, data)
+  File.open(file, 'w') do |f|
+    f.write(YAML.dump(data))
   end
 end
-data = ''
-index = []
-File.open('Data/Scripts.rvdata2', 'rb') do |file|
-  data = Marshal.load(file.read).map do |id, name, script|
+
+DATA_NAMES = %w{
+  Actors    Animations    Armors
+  Classes   CommonEvents  Enemies
+  Items     MapInfos      Skills
+  States    System        Tilesets
+  Troops    Weapons
+}
+DATA_NAMES.each do |name|
+  rvdata_filename = 'Data/' + name + '.rvdata2'
+  yaml_filename =   'Data/' + name + '.yml'
+
+  puts "Unpacking... #{name}"
+  data = load_rvdata2(rvdata_filename)
+  save_yaml(yaml_filename, data)
+end
+
+# Map
+Dir.glob('Data/Map[0-9][0-9][0-9].rvdata2').each do |rvdata_filename|
+  yaml_filename = 'Data/' + File.basename(rvdata_filename,'.rb') + '.yml'
+
+  puts "Unpacking... #{rvdata_filename}"
+  data = load_rvdata2(rvdata_filename)
+  save_yaml(yaml_filename, data)
+end
+
+def cleanup_scripts_rb
+  if File.exists?('Data/Scripts')
+    Dir.glob('Data/Scripts/*.rb') do |file|
+      File::delete(file)
+    end
+  else
+    Dir.mkdir('Data/Scripts')
+  end
+end
+
+# Scripts
+def unpack_scripts
+  data = load_rvdata2('Data/Scripts.rvdata2')
+
+  indexes = []
+  data.each do |id, name, script|
     name = '( NONAME )' if name.empty?
-    index << id
-    [name, '# -*- id: ' +
-           "#{id} -*-\n" +
-           Zlib::Inflate.inflate(script) +
-           "\n# -*- END_OF_SCRIPT -*-\n\n"
-    ]
+    #puts "Unpacking... Scripts/#{name}"
+    indexes << id
+
+    File.open('Data/Scripts/'+name+'.rb', 'wb') do |f|
+      f.write "# -*- id: #{id} -*-\n" +
+        Zlib::Inflate.inflate(script) +
+        "\n# -*- END_OF_SCRIPT -*-\n\n"
+    end
+  end
+
+  File.open('Data/Scripts.yml', 'wb') do |file|
+    file.write(YAML.dump(indexes))
   end
 end
-if File.exists?('Data/Scripts')
-  Dir.glob('Data/Scripts/*.rb') do |file|
-    File::delete(file)
-  end
-else
-  Dir.mkdir('Data/Scripts')
-end
-data.each do |name, script|
-  File.open('Data/Scripts/'+name+'.rb', 'ab') do |file|
-    file.write(script)
-  end
-end
-File.open('Data/Scripts.yml', 'wb') do |file|
-  file.write(YAML.dump(index))
-end
+
+puts "Unpacking... Scripts"
+cleanup_scripts_rb
+unpack_scripts
